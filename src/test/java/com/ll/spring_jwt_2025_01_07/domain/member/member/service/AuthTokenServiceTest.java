6d280a4b9a16ada9da47d849f5daf3daf9658676
solji <solji.choi89@gmail.com>
2025-01-07 12:13:18 +0900

    create jwt token

--- src/test/java/com/ll/spring_jwt_2025_01_07/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/ll/spring_jwt_2025_01_07/domain/member/member/service/AuthTokenServiceTest.java
@@ -1,11 +1,18 @@
 package com.ll.spring_jwt_2025_01_07.domain.member.member.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Key;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +27,29 @@ public class AuthTokenServiceTest {
     @DisplayName("authTokenService 서비스가 존재한다.")
     void t1() {
         assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jjwt로 JWT 생성")
+    void t2() {
+        int expireSeconds = 60 * 60 * 24 * 365;
+        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+
+        Claims claims = Jwts.claims()
+                .add("name", "Paul")
+                .add("age", 23)
+                .build();
+
+        String jwt = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+        System.out.println("jwt = " +jwt);
     }
 }
