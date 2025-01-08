3bbf69b5ce536674d8fa9b664f743f6eec25d626
solji <solji.choi89@gmail.com>
2025-01-08 09:46:22 +0900

    use securityUser

--- src/main/java/com/ll/spring_jwt_2025_01_07/global/rq/Rq.java
+++ src/main/java/com/ll/spring_jwt_2025_01_07/global/rq/Rq.java
@@ -48,10 +48,9 @@ public class Rq {
                         .getAuthentication()
         )
                 .map(Authentication::getPrincipal)
-                .filter(principal -> principal instanceof UserDetails)
-                .map(principal -> (UserDetails) principal)
-                .map(UserDetails::getUsername)
-                .flatMap(memberService::findByUsername)
+                .filter(principal -> principal instanceof SecurityUser)
+                .map(principal -> (SecurityUser) principal)
+                .map(securityUser -> new Member(securityUser.getId(), securityUser.getUsername()))
                 .orElse(null);
     }
 }
