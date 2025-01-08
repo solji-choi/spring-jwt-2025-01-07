078b24755a46687f7dfebd3bb955e47333695ea4
solji <solji.choi89@gmail.com>
2025-01-08 10:46:11 +0900

    WithUserDetails

--- src/main/java/com/ll/spring_jwt_2025_01_07/global/security/CustomUserDetailsService.java
+++ src/main/java/com/ll/spring_jwt_2025_01_07/global/security/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package com.ll.spring_jwt_2025_01_07.global.security;
 import com.ll.spring_jwt_2025_01_07.domain.member.member.entity.Member;
 import com.ll.spring_jwt_2025_01_07.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,7 +20,8 @@ public class CustomUserDetailsService implements UserDetailsService {
         Member member = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        return new User(
+        return new SecurityUser(
+                member.getId(),
                 member.getUsername(),
                 member.getPassword(),
                 List.of()
--- src/test/java/com/ll/spring_jwt_2025_01_07/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/ll/spring_jwt_2025_01_07/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -399,14 +400,12 @@ public class ApiV1PostControllerTest {
 
     @Test
     @DisplayName("글 삭제")
+    @WithUserDetails("user1")
     void t13() throws Exception {
-        Member actor = memberService.findByUsername("user1").get();
-        String actorAuthToken = memberService.getAuthToken(actor);
         Post post = postService.findById(1).get();
 
         ResultActions resultActions = mvc
                 .perform(delete("/api/v1/posts/1")
-                        .header("Authorization", "Bearer " + actorAuthToken)
                         .contentType(
                                 new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
                         )
